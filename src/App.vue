aadd41388f5d53eabd1bdb73f171a0cfde4e9a3d
hdk236p <hdk236p@edacom.co.kr>
2021-05-28 18:14:20 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box a">a</div>
+    <div class="box b">b</div>
+    <div class="box c">c</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// https://greensock.com/docs/v3/Plugins/ScrollTrigger
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  mounted(){
+    gsap.to(".c", {
+      scrollTrigger: {
+        trigger: ".c",
+        toggleActions: "restart pause reverse pause"
+      },
+      x: 400,
+      rotation: 360,
+      duration: 2
+    });
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height:4000px;
+}
+.box {
+  width:100px;
+  height:100px;
+  background:chartreuse;
+}
+.box + .box {
+  margin-top:200px;
 }
 </style>
